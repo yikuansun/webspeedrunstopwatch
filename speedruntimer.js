8885f5da9ad9a1301d068a5c128bad30ddc0d798
Yikuan Sun <yikuansun2015@gmail.com>
2021-01-10 13:22:13 -0500

    Update speedruntimer.js

--- speedruntimer.js
+++ speedruntimer.js
@@ -38,7 +38,7 @@ updateClock = function() {
 
 clockDisp = function() {
     s = (clock / 1000) % 60;
-    m = (clock / 1000) - s;
+    m = ((clock / 1000) - s) % 60;
     s_display = (s < 10)?("0"+s.toString()):(s.toString());
     time.innerHTML = m.toString() + ":" + s_display;
 };
